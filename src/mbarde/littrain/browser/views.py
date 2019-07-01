d2774bc0816529cff2266bbe9d4a32798923ed78
Matthias Barde <mbarde@uni-koblenz.de>
2019-07-01 18:24:00 +0200

    intermediate commit

--- src/mbarde/littrain/browser/views.py
+++ src/mbarde/littrain/browser/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from mbarde.littrain.epub import EPubReader
+from mbarde.littrain.lemma import LemmaCollector
+from mbarde.littrain.lemma import LemmaStorer
 from Products.Five.browser import BrowserView
 
 import sys
@@ -20,3 +22,20 @@ class ReadEPubView(BrowserView):
             return []
         self.ePubReader = EPubReader(self.context.file)
         return self.ePubReader.getChapters()
+
+    def getLemmas(self):
+        if self.context.portal_type != 'File':
+            return []
+
+        lemmaCollector = LemmaCollector()
+        ePubReader = EPubReader(self.context.file)
+
+        c = 0
+        for chapter in ePubReader.getChapters():
+            lemmaCollector.updateLemmasByChapter(chapter['content'], chapter['title'])
+            c += 1
+            if c > 0:
+                break
+
+        storer = LemmaStorer(self.context.title)
+        lemmaCollector.storeLemmas(storer)
